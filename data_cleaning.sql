dd95fadac2cc9c379c3f51c26aa3b4ed75ded206
matuszykw <matuszykwiktor@gmail.com>
2025-03-14 12:51:23 +0100

    add price column to order detail

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -19,3 +19,9 @@ WHERE item_name IS NULL OR price IS NULL;
 SELECT *
 FROM order_details
 WHERE order_time < '06:00:00' OR order_time > '23:59:59';
+
+ALTER TABLE order_details ADD COLUMN item_price DECIMAL(10,2);
+
+UPDATE order_details od
+JOIN menu_items mi ON od.item_id = mi.menu_item_id
+SET od.item_price = mi.price;
